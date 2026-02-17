12533086572644c18422655a5f331d282eb42615
Prahlad Somangoud Biradar <pb1083@srmist.edu.in>
2026-02-17 21:30:33 +0530

    uc2 assignment

---
+++ uc2.java
@@ -0,0 +1,16 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Each letter is 7 rows high and 9 characters wide
+
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  *****  ");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *     * ");
+        System.out.println("*       *" + "*       *" + " *     * " + "*       ");
+        System.out.println("*       *" + "*       *" + " ******  " + " *****   ");
+        System.out.println("*       *" + "*       *" + " *        " + "      *  ");
+        System.out.println(" *     * " + " *     * " + " *        " + " *     * ");
+        System.out.println("  *****  " + "  *****  " + " *        " + "  *****  ");
+
+    }
+}
